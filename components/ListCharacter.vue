797d06fac333c4e222f3d43e1fb85f83d944e667
Cobian04 <jancobian19@gmail.com>
2025-11-18 10:31:32 -0600

    Primer commit

---
+++ components/ListCharacter.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="characters">
+        <div class="characters_item">
+            <h2>Characters</h2>
+
+        </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue';
+import {useStore} from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    const chaacters = computed(() =>
+     store.state.charactersFilter);
+     
+     onMounted(() => {
+       store.dispatch('fetchCharacters');
+     });
+  }
+}
+</script>
+
+<style>
+
+</style>
